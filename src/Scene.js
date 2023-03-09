1c4b7908e253392d84ed3e7985afedfdf0182abf
Sinan Ucak <ucak.sinan@gmail.com>
2023-03-09 15:50:20 +0100

    tested with different models

--- src/Scene.js
+++ src/Scene.js
@@ -1,8 +1,10 @@
 import { useScroll, Text, OrbitControls } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
 import React, { useRef, useEffect, useState } from 'react'
+import { Model } from './Model'
 import { Helmet } from './Helmet'
 import { HelmetReduced } from './HelmetReduced'
+import { ModelTwo } from './ModelV2'
 
 function Scene({ scrollPos }) {
 
@@ -13,8 +15,9 @@ function Scene({ scrollPos }) {
   return (
     <React.Fragment>
       <axesHelper />
-      <Helmet scrollPos={scrollPos} />
-      <HelmetReduced scrollPos={scrollPos} />
+      <OrbitControls />
+      {/*<Helmet scrollPos={scrollPos} />*/}
+      <ModelTwo />
     </React.Fragment>
   )
 }
